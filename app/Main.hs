12ecbdaf17595902ca435e9a3ec9a95faa986c59
Max Oliynick <maksimolejn720@gmail.com>
2020-03-12 18:53:57 +0200

    Chapter 5. Ex 5-2

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import Chapter2.Lib
+import Data.Foldable (foldl')
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ show result
 
-
+result :: Integer
+result = foldl' (*) 1 [1 .. 100000]
